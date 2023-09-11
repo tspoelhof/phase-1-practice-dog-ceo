5d557fe4b20f64f2d8532df431c88fdcb96dc8e8
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-09-11 11:32:08 -0400

    dogs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+ fetch(imgUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        const imageUrls = data.message;
+
+        const imageContainer = document.getElementById("dog-image-container");
+
+        imageContainer.innerHTML = '';
+
+        imageUrls.forEach((imageUrl) => {
+            const img = document.createElement("img");
+            img.src = imageUrl;
+            img.alt = "Dog Image";
+            imageContainer.appendChild(img);
+        })
+    })
+    .catch((error) => {
+        console.error("Error fetching images:", error);
+    });
